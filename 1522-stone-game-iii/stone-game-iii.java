a4f9d0c726395709cba3613a8b8793f46f0bc0c7
srinivaschaudharyadusumalli-2654 <srinivaschaudharyadusumalli@gmail.com>
2026-08-03 22:43:26 +0530

    Time: 7 ms (88.35%) | Memory: 85.4 MB (76.70%) - LeetSync

---
+++ 1522-stone-game-iii/stone-game-iii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String stoneGameIII(int[] stoneValue) {
+        int n = stoneValue.length;
+        int[] dp = new int[n + 1];
+        for(int i =n -1; i>= 0;i--){
+            int best = Integer.MIN_VALUE;
+            int runningsum =0;
+            for (int k = 1; k <=3 ; k++){
+                if (i+k > n) break;
+                runningsum += stoneValue[i + k - 1];
+                best = Math.max(best , runningsum - dp[i+k]);
+            }
+            dp[i] =best;
+        }
+        if (dp[0]>0)return "Alice";
+        else if (dp[0] < 0) return "Bob";
+        else return "Tie";
+        
+    }
+}
